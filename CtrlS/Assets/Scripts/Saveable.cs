a52ae9f514483c73c54a04e57c98c4efbfe5dbdc
Herjeman <englundgustav@gmail.com>
2022-11-12 09:46:47 +0100

    Added Input system
    Also added events for saving and loading world

---
+++ CtrlS/Assets/Scripts/Saveable.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Saveable : MonoBehaviour
+{
+    private Transform _savedTransform;
+
+    private void Awake()
+    {
+        InputManager.OnSave += SaveTransform;
+        InputManager.OnLoad += LoadTransform;
+    }
+
+    private void SaveTransform()
+    {
+        _savedTransform = this.transform;
+    }
+
+    private void LoadTransform()
+    {
+        this.transform.position = _savedTransform.position;
+        this.transform.rotation = _savedTransform.rotation;
+        this.transform.localScale = _savedTransform.localScale;
+    }
+
+    private void OnDestroy()
+    {
+        InputManager.OnSave -= SaveTransform;
+        InputManager.OnLoad -= LoadTransform;
+    }
+}
