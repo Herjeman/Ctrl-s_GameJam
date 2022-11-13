46cbc3956c876c7e9490fa998ab307c9f1df8151
Decasoul <johannes.perdal@edu.futuregames.nu>
2022-11-13 10:55:58 +0100

    SoundManager got 5 sfxs

--- CtrlS/Assets/Scripts/SoundManager.cs
+++ CtrlS/Assets/Scripts/SoundManager.cs
@@ -1,20 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Diagnostics;
 
 public class SoundManager : MonoBehaviour
 {
-    [SerializeField] private AudioClip Jump;
-    [SerializeField] private AudioClip PuzzleSolved;
+    [SerializeField] private AudioClip _jump;
+    [SerializeField] private AudioClip _puzzleSolved;
+    [SerializeField] private AudioClip _ctrlS;
+    [SerializeField] private AudioClip _ctrlZ;
+    [SerializeField] private AudioClip _teleport;
     private AudioSource _audioSource;
 
     public void JumpSFX()
     {
-        _audioSource.PlayOneShot(Jump);
+        _audioSource.PlayOneShot(_jump);
     }
     public void PuzzleSolvedSFX()
     {
-        _audioSource.PlayOneShot(PuzzleSolved);
+        _audioSource.PlayOneShot(_puzzleSolved);
+    }
+    public void CtrlSSFX()
+    {
+        _audioSource.PlayOneShot(_ctrlS);
+    }
+    public void CtrlZSFX()
+    {
+        _audioSource.PlayOneShot(_ctrlZ);
+    }
+    public void TeleportSFX()
+    {
+        _audioSource.PlayOneShot(_teleport);
     }
     private void Start()
     {
